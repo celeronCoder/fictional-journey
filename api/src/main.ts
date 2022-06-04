7ad8d98324a5b830cea66f5602aee718a22accb5
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-06-04 15:39:53 +0000

    feat: added swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import compression from 'fastify-compress';
 import { AppModule } from './app.module';
 
@@ -11,6 +12,19 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  // swagger setup
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Todos API')
+    .setDescription('REST API for Todos')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  // compresion
   await app.register(compression, { encodings: ['gzip'] });
   await app.listen(3000);
 }
